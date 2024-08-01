77b4740308617e7cf80481ea2b3241b25393665a
Kukuliak-Dmytro <kukuliak.dmytro.clg@chnu.edu.ua>
2024-08-01 11:31:17 +0300

    Experimenting

--- tattooSaloon/src/Catalog/Catalog.jsx
+++ tattooSaloon/src/Catalog/Catalog.jsx
@@ -7,6 +7,8 @@ import Item from "./Item";
 import { BiSearch } from "react-icons/bi";
 import { useParams, Link } from "react-router-dom";
 
+import imgCut from '../assets/cut@0,5x.png';
+
 
 export default function Catalog() {
   const { pageNo } = useParams();
@@ -27,7 +29,7 @@ export default function Catalog() {
   return (
     <>
       <Header />
-      <Container src="../src/assets/cut@0,5x.png" height={250} justify="center">
+      <Container src={imgCut} height={250} justify="center">
         <div className={styles.searchContainer}>
           <form action="search">
             <input type="text" className={styles.search} placeholder="Search" />
